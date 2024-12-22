e5b2a4b8829517afba806016cb154a3d7cb24cf9
Tehnolog3000 <tehnolog3000@gmail.com>
2024-12-22 19:19:13 +0200

    hlegal5

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ document.getElementById('burger').addEventListener('click', () => {
   document.getElementById('closeMenu').addEventListener('click', () => {
     document.getElementById('menu').classList.remove('menu--open');
   });
+  document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".tabs-column li");
+  
+    $(function () {
+      $("#tabs-mobile").tabs({
+        activate: function (event, ui) {
+        
+          $("#tabs-mobile .tabs-column-mobile li").removeClass("active");
+          ui.newTab.addClass("active");
+        },
+      });
+    });
+    
+ 
+    $(function () {
+      $("#tabs-desktop").tabs({
+        activate: function (event, ui) {
+         
+          $("#tabs-desktop .tabs-column li").removeClass("active");
+          
+          ui.newTab.addClass("active");
+        },
+      });
+    });
+  });
